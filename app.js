d6e1d7ed0ae3477adb967b5b6b486612778d0c2d
Lucas Churchill <lucas@agtlucas.com>
2014-06-11 15:21:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var nodeCorreios = require('node-correios'),
   extend = require('extend'),
   correios = new nodeCorreios();
 
+/*
+ * Códigos do serviço
+
+  40010 = SEDEX Varejo
+  40045 = SEDEX a Cobrar Varejo
+  40215 = SEDEX 10 Varejo
+  40290 = SEDEX Hoje Varejo
+  41106 = PAC Varejo
+
+*/
 var args = {
     nCdServico: '41106',
     sCepOrigem: '89254105',
