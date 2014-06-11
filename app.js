2be5ac7c4cf8beb65189ab9b5dec2811383b606d
Lucas Churchill <lucas@agtlucas.com>
2014-06-11 15:09:39 -0300

    Updates

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 'use strict';
 
 var nodeCorreios = require('node-correios'),
+  extend = require('extend'),
   correios = new nodeCorreios();
 
+var args = {
+    nCdServico: '41106',
+    sCepOrigem: '89254105',
+    sCepDestino: '30110013',
+    nvlPeso: '2',
+    nCdFormato: 1,
+    nVlComprimento: 200,
+    nVlAltura: 200,
+    nVlLargura: 200,
+    nVlDiametro: 100
+};
+
 // Executa o método de pesquisa de valor de frete
-correios.calcPreco(args);
+correios.calcPreco(args, function (result) {
+  console.log(result);
+});
 
 // Quando o result for emitido faz um log do retorno da api
 correios.on('result', function (result) {
