b1777379b4e38f8beee170eda82c5c8cb81bc0ee
Lucas Churchill <lucas@agtlucas.com>
2014-06-11 15:17:38 -0300

    Updates

--- app.js
+++ app.js
@@ -8,25 +8,15 @@ var args = {
     nCdServico: '41106',
     sCepOrigem: '89254105',
     sCepDestino: '30110013',
-    nvlPeso: '2',
+    nVlPeso: '2',
     nCdFormato: 1,
-    nVlComprimento: 200,
-    nVlAltura: 200,
-    nVlLargura: 200,
-    nVlDiametro: 100
+    nVlComprimento: 20.0,
+    nVlAltura: 20.0,
+    nVlLargura: 20.0,
+    nVlDiametro: 10.0
 };
 
 // Executa o método de pesquisa de valor de frete
 correios.calcPreco(args, function (result) {
   console.log(result);
-});
-
-// Quando o result for emitido faz um log do retorno da api
-correios.on('result', function (result) {
-  console.log(result);
-});
-
-// Se ocorreu algum erro na execução, faz um log do erro
-correios.on('error', function (err) {
-  console.log(err);
 });
